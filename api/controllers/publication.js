2dd670631ec6017fd7922ce6540197ba4fb9fa8b
rafaelRamirez <rafar1129@gmail.com>
2025-12-08 17:43:11 -0500

    feat:publication method

--- api/controllers/publication.js
+++ api/controllers/publication.js
@@ -15,6 +15,34 @@ function probando(req, res) {
     });
 }
 
+function savePublication(req, res) {
+  const params = req.body;
+
+  if (!params.text) {
+    return res.status(200).send({ message: "Debes enviar un texto!!" });
+  }
+
+  const publication = new Publication();
+  publication.text = params.text;
+  publication.file = null;
+  publication.user = req.user.sub;
+  publication.created_at = moment().unix();
+
+  publication.save((err, publicationStored) => {
+    if (err) {
+      return res.status(500).send({ message: 'Error al guardar la publicación' });
+    }
+
+    if (!publicationStored) {
+      return res.status(404).send({ message: 'La publicación no se ha guardado' });
+    }
+
+    return res.status(200).send({ publication: publicationStored });
+  });
+}
+
+
 module.exports = {
-    probando
+    probando,
+    savePublication
 };
