7b5c4da6cca7a5eff6edbece808808289bfe1ca4
rafaelRamirez <rafar1129@gmail.com>
2025-12-13 11:10:09 -0500

    feat:A large part of the view was added

---
+++ freyla-web/src/typings.d.ts
@@ -0,0 +1,9 @@
+// Global typings for third-party scripts loaded via angular.json
+
+declare const jQuery: any;
+declare const $: any;x
+declare const module: NodeModule;
+
+interface NodeModule {
+  id: string;
+}
